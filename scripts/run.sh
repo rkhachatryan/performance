f3f2ba4e9dce48f095de4e4e0ea16fba0e1491a8
Nico Kruber <nico@data-artisans.com>
2018-01-19 15:04:31 +0100

    [flink][script] update/improve run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
 
 RUNNAME=$1
-FLINK_DIR=/home/robert/flink/build-target
-LOG="run-log-$1"
+FLINK_BIN=${2:-flink}
+JOB_JAR=${3:-flink-jobs-0.1-SNAPSHOT.jar}
+ANALYZE_JAR=${4:-perf-common-0.1-SNAPSHOT-jar-with-dependencies.jar}
+
+NODES=${5:-10 20 30}
+BUFFER_TIMEOUTS_THROUGHPUT=${6:-0 5 10}
+BUFFER_TIMEOUTS_LATENCY=${6:-0 10 100}
+
+LOG="run-log-${RUNNAME}"
 
 export HADOOP_CONF_DIR=/etc/hadoop/conf
-TIMEOUT=100
+CREDIT_BASED=true
+BUFFER_TIMEOUT=100
 REPART=1
-BUFFERS=2048
-SLEEP_FREQ=0
-DELAY=0
-FT=""
+SOURCE_DELAY=0 # in ms
+SOURCE_DELAY_FREQ=0 # every this many records
+LATENCY_MEASURE_FREQ=100000 # every this many records
+LOG_FREQ=100000 # every this many records
+PAYLOAD_SIZE=12
+CHECKPOINTING_INTERVAL=""
+SLOTS=4
 start_job() {
-	echo -n "$1;$REPART;$FT;$BUFFERS;$TIMEOUT;" >> $LOG
-	echo "Starting job on YARN with $1 workers and a timeout of $TIMEOUT ms"
-	PARA=`echo $1*4 | bc`
+	nodes=$1
+	echo -n "$nodes;$SLOTS;$REPART;$CHECKPOINTING_INTERVAL;$BUFFER_TIMEOUT;$SOURCE_DELAY;$SOURCE_DELAY_FREQ;$LATENCY_MEASURE_FREQ;$LOG_FREQ;$PAYLOAD_SIZE;" >> $LOG
+	echo "Starting job on YARN with $nodes workers and a buffer timeout of $BUFFER_TIMEOUT ms (source delay $SOURCE_DELAY)"
+	PARA=$(($1*$SLOTS))
 	CLASS="com.github.projectflink.streaming.Throughput"
-	$FLINK_DIR/bin/flink run -m yarn-cluster -yn $1 -yst -yD taskmanager.network.numberOfBuffers=$BUFFERS -yjm 768 -ytm 3072 -ys 4 -yd -p $PARA -c $CLASS /home/robert/flink-perf/flink-jobs/target/flink-jobs-0.1-SNAPSHOT.jar $FT --sleepFreq $SLEEP_FREQ --repartitions $REPART --timeout $TIMEOUT --para 160 --payload 12 --delay $DELAY --logfreq 1000000 --sourceParallelism 160 --sinkParallelism 160 --latencyFreq 1000000 | tee lastJobOutput
+	"${FLINK_BIN}" run -m yarn-cluster -yn $1 -yst -yD taskmanager.network.credit-based.enabled=$CREDIT_BASED -yjm 768 -ytm 3072 -ys $SLOTS -yd -p $PARA -c $CLASS "${JOB_JAR}" $CHECKPOINTING_INTERVAL --sleepFreq $SOURCE_DELAY_FREQ --repartitions $REPART --timeout $BUFFER_TIMEOUT --payload $PAYLOAD_SIZE --delay $SOURCE_DELAY --logfreq $LOG_FREQ --latencyFreq $LATENCY_MEASURE_FREQ | tee lastJobOutput
 }
 
 append() {
@@ -42,42 +54,40 @@ kill_on_yarn() {
 
 getLogsFor() {
 	sleep 30
-	yarn logs -applicationId $1 > logs/$1
+	yarn logs -applicationId $1 > logs/${RUNNAME}-$1
 }
 analyzeLogs() {
-	java -cp /home/robert/flink-perf/perf-common/target/perf-common-0.1-SNAPSHOT-jar-with-dependencies.jar com.github.projectflink.common.AnalyzeTool logs/$1 >> $LOG
+	java -cp "${ANALYZE_JAR}" com.github.projectflink.common.AnalyzeTool logs/${RUNNAME}-$1 >> ${LOG}
 }
 
 function experiment() {
-	start_job $1
-	duration $2
+	name=$1
+	nodes=$2
+	dur=$3
+	start_job ${nodes}
+	duration ${dur}
 	APPID=`kill_on_yarn`
 
-	getLogsFor $APPID
-	analyzeLogs $APPID
+	getLogsFor ${name}-${APPID}
+	analyzeLogs ${name}-${APPID}
 }
 
-echo "machines;duration-sec;yarnAppId;lat-mean;lat-median;lat-90percentile;throughput-mean;throughput-max;latencies;throughputs" >> $LOG
+mkdir -p logs
 
-DURATION=900
+echo "machines;slots;repartitions;checkpointing_interval;buffer_timeout;source_delay;source_delay_freq;latency_measure_freq;log_freq;payload_size;duration-sec;jobId;machines_results;lat-mean;lat-median;lat-90percentile;lat-95percentile;lat-99percentile;throughput-mean;throughput-max;latencies;throughputs" >> $LOG
+
+# DURATION=900 # 15min
+DURATION=300 # 5min
 
 #redo throughput tests
-REPART=1
-TIMEOUT=5
+REPART=1 # only one re-partitioning via a (hashing) key-by
+BUFFER_TIMEOUT=5 # default 5ms
 
-experiment 10 $DURATION
-experiment 10 $DURATION
-experiment 10 $DURATION
-
-
-experiment 20 $DURATION
-experiment 20 $DURATION
-experiment 20 $DURATION
-
-
-experiment 30 $DURATION
-experiment 30 $DURATION
-experiment 30 $DURATION
+for nodes in $NODES; do
+	experiment throughput1 $nodes $DURATION
+	experiment throughput1 $nodes $DURATION
+	experiment throughput1 $nodes $DURATION
+done
 
 exit
 
@@ -85,72 +95,31 @@ exit
 
 REPART=2
 
-TIMEOUT=5
+for CHECKPOINTING_INTERVAL in "" " --ft 1000 "; do
+	for BUFFER_TIMEOUT in $BUFFER_TIMEOUTS_THROUGHPUT; do
+		for nodes in $NODES; do
+			experiment throughput2 $nodes $DURATION
+			experiment throughput2 $nodes $DURATION
+			experiment throughput2 $nodes $DURATION
+		done
+	done
+done
 
-BUFFERS=4096
-
-experiment 10 $DURATION
-experiment 10 $DURATION
-experiment 10 $DURATION
-
-
-experiment 20 $DURATION
-experiment 20 $DURATION
-experiment 20 $DURATION
-
-
-experiment 30 $DURATION
-experiment 30 $DURATION
-experiment 30 $DURATION
-
-FT=" --ft 1000 "
-
-experiment 10 $DURATION
-experiment 10 $DURATION
-experiment 10 $DURATION
-
-
-experiment 20 $DURATION
-experiment 20 $DURATION
-experiment 20 $DURATION
-
-
-experiment 30 $DURATION
-experiment 30 $DURATION
-experiment 30 $DURATION
-
-FT=""
-TIMEOUT=0
-
-experiment 10 $DURATION
-experiment 10 $DURATION
-experiment 10 $DURATION
-
-
-experiment 20 $DURATION
-experiment 20 $DURATION
-experiment 20 $DURATION
-
-
-experiment 30 $DURATION
-experiment 30 $DURATION
-experiment 30 $DURATION
-
-FT=" --ft 1000 "
-
-experiment 10 $DURATION
-experiment 10 $DURATION
-experiment 10 $DURATION
-
-
-experiment 20 $DURATION
-experiment 20 $DURATION
-experiment 20 $DURATION
-
-
-experiment 30 $DURATION
-experiment 30 $DURATION
-experiment 30 $DURATION
+# latency benchmark
+SOURCE_DELAY=100 # in ms
+SOURCE_DELAY_FREQ=1 # every this many records
+for BUFFER_TIMEOUT in $BUFFER_TIMEOUTS_LATENCY; do
+  for SOURCE_DELAY in 1 5 10 50 100; do
+	for nodes in $NODES; do
+		# roughly once per second
+		LOG_FREQ=$(($SOURCE_DELAY_FREQ*1000/$SOURCE_DELAY)) # every this many records
+		LATENCY_MEASURE_FREQ=$(($LOG_FREQ/5))
+		experiment latency2 $nodes $DURATION
+		experiment latency2 $nodes $DURATION
+		experiment latency2 $nodes $DURATION
+	done
+  done
+done
 
 exit
 
